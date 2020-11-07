b06df904c9df6a16b1bbec9735a03d57a8a82980
Justin Marshall <justinmarshall20@gmail.com>
2020-11-06 18:48:11 -0800

    Disabled d3d12 debug spew.

--- code/rhi/d3d12/d3d12_main.cpp
+++ code/rhi/d3d12/d3d12_main.cpp
@@ -390,16 +390,16 @@ void GL_Init(HWND hwnd, HINSTANCE hinstance, int width, int height)
 #if defined(_DEBUG)
 	// Enable the debug layer (requires the Graphics Tools "optional feature").
 	// NOTE: Enabling the debug layer after device creation will invalidate the active device.
-	{
-		ComPtr<ID3D12Debug> debugController;
-		if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&debugController))))
-		{
-			debugController->EnableDebugLayer();
-	
-			// Enable additional debug layers.
-			dxgiFactoryFlags |= DXGI_CREATE_FACTORY_DEBUG;
-		}
-	}
+	//{
+	//	ComPtr<ID3D12Debug> debugController;
+	//	if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&debugController))))
+	//	{
+	//		debugController->EnableDebugLayer();
+	//
+	//		// Enable additional debug layers.
+	//		dxgiFactoryFlags |= DXGI_CREATE_FACTORY_DEBUG;
+	//	}
+	//}
 #endif
 
 	ComPtr<IDXGIFactory4> factory;
